3b54009271df7121895d9213a16e7d2a63d366eb
Mikhail Work <soundgot@gmail.com>
2026-09-10 09:47:20 +0500

    commit

--- commit.ps1
+++ commit.ps1
@@ -21,3 +21,6 @@ if (-not $tortoiseExe) {
 
 Start-Process -FilePath $tortoiseExe -ArgumentList "/command:commit /path:."
 Write-Host "Commit dialog opened for: $(Get-Location)"
+
+cd $env:localappdata\ETS2_Assist\map_overrides\ets2_overrides
+git commit -m "Upd" *> $null; git push *> $null
