2695b46167f5f358b6ed71b708ed741f428d155e
Mikhail Work <soundgot@gmail.com>
2026-09-10 14:40:23 +0500

    pull

--- pull.ps1
+++ pull.ps1
@@ -10,6 +10,4 @@ cd $PSScriptRoot
 
 dotnet clean
 dotnet restore
-dotnet publish -c Release
-
-Start-Process "$PSScriptRoot\bin\Release\net10.0-windows\win-x64\publish\ETS2_Assist.exe" -Verb RunAs
+ .\compile.ps1
