72dcabcda41bc07858e2b287a9f60a14e8a8388f
Mikhail Nikitin <soundgot@gmail.com>
2026-09-19 11:31:03 +0500

    Update BuildInfo fallback for RAW-INPUT-TEST build

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -19,7 +19,7 @@ namespace ETS2_Assist_GUI
                         return attr.InformationalVersion;
                 }
                 catch { }
-                return "1.0.40.65-LOGS-COLLECT-BTN";
+                return "1.0.40.66-RAW-INPUT-TEST";
             }
         }
     }
