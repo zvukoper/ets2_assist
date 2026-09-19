6135a7c5394784700f3ea9f7d9711372f4acd5fb
Mikhail Nikitin <soundgot@gmail.com>
2026-09-19 11:40:46 +0500

    Update build fallback to 1.0.40.67 RAW-INPUT-TEST-R2

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -19,7 +19,7 @@ namespace ETS2_Assist_GUI
                         return attr.InformationalVersion;
                 }
                 catch { }
-                return "1.0.40.66-RAW-INPUT-TEST";
+                return "1.0.40.67-RAW-INPUT-TEST-R2";
             }
         }
     }
