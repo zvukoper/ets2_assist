87193da1d054871cd1185f0a3ae0bfc54d226131
Mikhail Nikitin <soundgot@gmail.com>
2026-09-19 14:26:06 +0500

    v1.0.40.81: fix Quest JavaScript comment syntax

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -19,7 +19,7 @@ namespace ETS2_Assist_GUI
                         return attr.InformationalVersion;
                 }
                 catch { }
-                return "1.0.40.80-CURSOR-CAL-TAB-QUEST-R10";
+                return "1.0.40.81-CURSOR-CAL-TAB-QUEST-R11";
             }
         }
     }
