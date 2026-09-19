70ee43944024d86eee787768442f21cb7ab94e2c
Mikhail Nikitin <soundgot@gmail.com>
2026-09-19 12:03:07 +0500

    Update build fallback to 1.0.40.69

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -19,7 +19,7 @@ namespace ETS2_Assist_GUI
                         return attr.InformationalVersion;
                 }
                 catch { }
-                return "1.0.40.68-WEBOVERLAY-VERSION";
+                return "1.0.40.69-WEBOVERLAY-VERSION";
             }
         }
     }
