8534ed29aeb6a1278894d3dfd849447c514b4c15
Mikhail Nikitin <soundgot@gmail.com>
2026-09-19 12:35:42 +0500

    Update software cursor version fallback

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -19,7 +19,7 @@ namespace ETS2_Assist_GUI
                         return attr.InformationalVersion;
                 }
                 catch { }
-                return "1.0.40.71-SOFT-CURSOR-R1";
+                return "1.0.40.72-SOFT-CURSOR-R2";
             }
         }
     }
