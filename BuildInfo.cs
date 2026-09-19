5cb6aceaade1a5aa4f6a90d9b6df6f28a49c7f7f
Mikhail Nikitin <soundgot@gmail.com>
2026-09-19 17:12:35 +0500

    v1.0.40.83: sync build version after ESC UI fixes

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -19,7 +19,7 @@ namespace ETS2_Assist_GUI
                         return attr.InformationalVersion;
                 }
                 catch { }
-                return "1.0.40.82-ESC-SAFEZONE-QUEST-R12";
+                return "1.0.40.83-ESC-SAFEZONE-QUEST-R13";
             }
         }
     }
