3328b5bd9c9286a677732f5c53e31fec13b3f912
Mikhail Nikitin <soundgot@gmail.com>
2026-09-19 13:56:48 +0500

    v1.0.40.79: bump application build version

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -19,7 +19,7 @@ namespace ETS2_Assist_GUI
                         return attr.InformationalVersion;
                 }
                 catch { }
-                return "1.0.40.78-QUEST-CURSOR-TAB-CLICK-R8";
+                return "1.0.40.79-CURSOR-CAL-TAB-QUEST-R9";
             }
         }
     }
